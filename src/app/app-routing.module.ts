fd43988d37dac3256cf9aa68092b8496a9d1a0a4
ivanovl <ouromeo@yandex.ru>
2019-09-30 14:27:10 +0300

    add news and shop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent ,
-  },
   { path: 'test',
     loadChildren: () => import( './modules/test/test.module' ).then(
       m => m.TestModule),
